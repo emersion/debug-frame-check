026b3321712a61bd614d596a43afd4698fd02ea6
emersion <contact@emersion.fr>
2018-02-26 16:15:56 +0100

    Add bin/dwarfcmp.py

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # debug-frame-check
-Check .eh_frame and .debug_frame information
+
+Check `.eh_frame` and `.debug_frame` information.
+
+## ORC & DWARF crosscheck
+
+Dependencies:
+* `objtool` from the Linux kernel tree
+* [`dareog`](https://github.com/emersion/dareog)
+* [`pyelftools`](https://github.com/eliben/pyelftools)
+
+Manual usage:
+
+```shell
+# Compile the file with DWARF debugging information
+gcc -c test.c -o test-dwarf.o
+gcc -no-pie test-dwarf.o -o test-dwarf
+
+# Compile the file with ORC debugging information and convert ORC to DWARF
+gcc -fno-asynchronous-unwind-tables -c test.c -o test-orc.o
+objtool orc generate test-orc.o
+dareog generate-dwarf test-orc.o
+gcc -no-pie test-orc.o -o test-orc
+
+# Compare DWARF tables
+bin/dwarfcmp.py test-dwarf test-orc
+```
+
+## License
+
+MIT
---
+++ bin/dwarfcmp.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import operator
+import argparse
+from elftools.elf.elffile import ELFFile
+from elftools.dwarf.descriptions import (
+    set_global_machine_arch,
+    describe_CFI_register_rule,
+    describe_CFI_CFA_rule,
+)
+from elftools.dwarf.callframe import FDE, CFARule, RegisterRule
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--strict', help='enable strict mode', action='store_true')
+parser.add_argument('files', nargs='+')
+
+args = parser.parse_args()
+strict = args.strict
+filenames = args.files
+
+def compare_CFI_CFA_rule(a, b):
+    if a.expr or b.expr:
+        if not (a.expr and b.expr):
+            return False
+        return not strict # TODO
+    else:
+        return a.reg == b.reg and a.offset == b.offset
+
+def compare_CFI_register_rule(a, b):
+    if not strict and (a.type == 'UNDEFINED' or b.type == 'UNDEFINED'):
+        return True
+    return a.type == b.type and a.arg == b.arg
+
+elf_files = [ELFFile(open(filename, 'rb')) for filename in filenames]
+
+machine_arch = None
+for elf_file in elf_files:
+    arch = elf_file.get_machine_arch()
+    if machine_arch is None:
+        machine_arch = arch
+    elif machine_arch != arch:
+        raise "Cannot compare ELF files with different machine architectures"
+set_global_machine_arch(machine_arch)
+
+# TODO: optimize dwarf_tables data type
+dwarf_tables = []
+pcs = set()
+for elf_file in elf_files:
+    if not elf_file.has_dwarf_info():
+        raise "ELF file is missing DWARF info"
+
+    dwarf_info = elf_file.get_dwarf_info()
+    dwarf_table = {}
+    cfi_entries = None
+    if dwarf_info.has_EH_CFI():
+        cfi_entries = dwarf_info.EH_CFI_entries()
+    # TODO: .debug_frame
+    if cfi_entries is None:
+        raise "ELF file is missing CFI entries"
+
+    # TODO: sorted insertion
+    dwarf_table = []
+    for entry in cfi_entries:
+        if isinstance(entry, FDE):
+            decoded_table = entry.get_decoded()
+            for line in decoded_table.table:
+                line['reg_order'] = decoded_table.reg_order
+                dwarf_table.append(line)
+                pcs.add(line['pc'])
+    sorted(dwarf_table, key=operator.itemgetter('pc'))
+
+    dwarf_tables.append(dwarf_table)
+
+pcs = sorted(pcs)
+
+mismatched = False
+for pc in pcs:
+    ref_line = {}
+    for dwarf_table in dwarf_tables:
+        line = None
+        while len(dwarf_table) > 0:
+            line = dwarf_table[0]
+            if line['pc'] > pc:
+                line = None
+                break
+            if len(dwarf_table) < 2:
+                break
+            next_line = dwarf_table[1]
+            if next_line['pc'] > pc:
+                break
+            dwarf_table.pop(0)
+        if line is None:
+            continue
+
+        if 'cfa' not in ref_line:
+            ref_line['cfa'] = line['cfa']
+        else:
+            if not compare_CFI_CFA_rule(ref_line['cfa'], line['cfa']):
+                print("CFA rule mismatch at pc=0x%x: %s vs %s" % (
+                    pc,
+                    describe_CFI_CFA_rule(ref_line['cfa']),
+                    describe_CFI_CFA_rule(line['cfa']),
+                ))
+                mismatched = True
+
+        for reg_num in line['reg_order']:
+            reg_rule = line.get(reg_num, RegisterRule(RegisterRule.UNDEFINED))
+            if reg_num not in ref_line:
+                ref_line[reg_num] = reg_rule
+            else:
+                if not compare_CFI_register_rule(ref_line[reg_num], reg_rule):
+                    print("Register rule mismatch at pc=0x%x: %s vs %s" % (
+                        pc,
+                        describe_CFI_register_rule(ref_line[reg_num]),
+                        describe_CFI_register_rule(reg_rule),
+                    ))
+                    mismatched = True
+
+if mismatched:
+    exit(1)
+else:
+    print("All green.")
